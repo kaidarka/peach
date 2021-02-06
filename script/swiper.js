f8f82e05f9e4026d92bdd5603284e659e4878f55
kaidarka <70837397+kaidarka@users.noreply.github.com>
2021-02-06 15:11:00 +0300

    слайдер и скролл

---
+++ script/swiper.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+  
+
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.modal-slider__next',
+      prevEl: '.modal-slider__prev',
+    },
+  
+  });
